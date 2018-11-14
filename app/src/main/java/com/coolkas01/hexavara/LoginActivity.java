6dc005831930e805d87304e4fff804009d42cb3b
Andika Aji Prabowo <andika.aji04@gmail.com>
2018-11-15 02:01:25 +0700

    Setup login activity and add library

--- app/src/main/java/com/coolkas01/hexavara/LoginActivity.java
+++ app/src/main/java/com/coolkas01/hexavara/LoginActivity.java
@@ -2,6 +2,8 @@ package com.coolkas01.hexavara;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText eUsername = (EditText) findViewById(R.id.edt_username);
+        EditText ePassword = (EditText) findViewById(R.id.edt_password);
+        Button bLogin = (Button) findViewById(R.id.btn_login);
     }
 }
